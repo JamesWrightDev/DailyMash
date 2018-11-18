5156c8d504044e5ee5749a53e534fa09916f2748
JamesWrightDev <32226552+JamesWrightDev@users.noreply.github.com>
2018-11-18 22:48:27 +0000

    fix grid system

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 var gulp = require('gulp');
-var gulp = require('gulp');
 var sass = require('gulp-sass');
+
 
 gulp.task('sass', function () {
   gulp.src('./scss/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./scss/style.scss', ['sass']);
+  gulp.watch('./scss/**/*.scss', ['sass']);
 });
